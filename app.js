94fd6a3704f6587468ad643c5dd8df14690ecfe2
PhucBinh <phucbinh.2001@gmail.com>
2020-05-12 16:19:24 +0700

    Them chuc nang cho nut huong dan

--- app.js
+++ app.js
@@ -134,3 +134,24 @@ function offnav() {
     navbarrg.style.display = "none";
 }
 // navbar
+
+// Phúc Bình JS
+var boxgiasu = document.getElementById('giasu');
+var boxhocvien = document.getElementById('hocvien');
+var btngiasu = document.getElementById('btngiasu');
+var btnhocvien = document.getElementById('btnhocvien')
+
+function giasu() {
+    boxhocvien.style.display = "none";
+    boxgiasu.style.display = "block";
+    btnhocvien.classList.remove('active');
+    btngiasu.classList.add('active');
+}
+
+function hocvien() {
+    boxgiasu.style.display = "none";
+    boxhocvien.style.display = "block";
+    btnhocvien.classList.add('active');
+    btngiasu.classList.remove('active');
+}
+
